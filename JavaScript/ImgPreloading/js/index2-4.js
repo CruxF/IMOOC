1a045479fdc833cd139c76d1285e000a9d4809ca
James Wenming <792031405@qq.com>
2018-06-25 21:36:58 +0800

    图片预加载

---
+++ JavaScript/ImgPreloading/js/index2-4.js
@@ -0,0 +1,42 @@
+// 图片预加载
+(function($) {
+  function PreLoad(imgs, options) {
+    this.imgs = (typeof imgs === 'string') ? [imgs] : imgs;
+    this.opts = $.extend({}, Preload.DEFAULTS, options);
+    this._unoredered();
+  }
+  Preload.DEFAULTS = {
+    // 每一张图片加载完毕后执行
+    each: null,
+    // 所有图片加载完毕后执行
+    all: null
+  };
+  Preload.prototype._unoredered = function() {
+    var imgs = this.imgs;
+    var opts = this.opts;
+    var count = 0;
+    var len = imgs.length
+    $.each(imgs, function(i, src) {
+      if(typeof src != 'string') {
+        return
+      }
+      var imgObj = new Image();
+      $(imgObj).on('load error', function() {
+        opts.each && opts.each(count);
+        if(count >= len - 1) {
+          opts.all && opts.all();
+        }
+        count++;
+      });
+      imgObj.src = src;
+    });
+  }
+  // 两种插件的不同调用方式
+  // 1、$.fn.extend ==> $('#img').preload()
+  // 2、$.extend ==> $.preload()
+  $.extend({
+    preload: function(imgs, opts) {
+      new PreLoad(imgs, opts);
+    }
+  });
+})(jQuery)
