a908851e5253b973db90a83bfcd36c998a4c9a12
cruxf <929762930@qq.com>
2019-02-15 10:37:17 +0800

    React组件

---
+++ React/ReactComponent/src/firstDemo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FirstDemo extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>I am Typler</h1>
+      </div>
+    );
+  }
+}
+
+export default FirstDemo;
